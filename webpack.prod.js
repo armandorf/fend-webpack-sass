2d2ffd2e04fe39f6d6be0ed7041ce59772279078
Pedro Flores <pflores@apttus.com>
2019-09-25 18:57:56 -0700

    Update prod webpack config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,15 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        // can name 'library' to any name we choose
+        // all our JS code to be accessible through this Client library
+        // webpack let's takes care of this
+        library: 'Client'
+    },
     mode: 'production',
     module: {
         rules: [
@@ -11,7 +20,13 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.scss$/,
+                // chained loaders (run from right to left) <===
+                use: ['style-loader', 'css-loader', 'sass-loader']
+                //         3rd             2nd            1st
+            },
         ]
     },
     plugins: [
